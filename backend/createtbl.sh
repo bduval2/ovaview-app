bb9b314bdba3965fd04b3d7e53c40014e5c159f0
Beatrice Duval <beatriceduval@MacBook-Air-de-Beatrice.local>
2023-03-23 00:21:10 -0400

    #29-#30: change user identifiers in tables to be of type TEXT

--- backend/createtbl.sh
+++ backend/createtbl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sqlite3 periodTracking.db  "create table logs (id_crypt INTEGER PRIMARY KEY, entry TEXT);"
-sqlite3 periodTracking.db  "create table users (id INTEGER PRIMARY KEY);"
+sqlite3 periodTracking.db  "create table logs (id_crypt TEXT PRIMARY KEY, entry TEXT);"
+sqlite3 periodTracking.db  "create table users (id_crypt TEXT PRIMARY KEY);"
