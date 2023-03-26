9c1d7bea863ec4a1351f5f1d160ddf4540736932
Beatrice Duval <beatriceduval@MacBook-Air-de-Beatrice.local>
2023-03-26 15:09:34 -0400

    #51 - add backend functionality (and test function) to remove individual logs

--- WebApp/backEnd/dashboard.php
+++ WebApp/backEnd/dashboard.php
@@ -81,8 +81,34 @@
         }
     }
 
-    function removeLog($user_id, $mood, $symptoms, $note, $year, $month, $date) {
+    function deleteLog($user_id, $year, $month, $date) {
+          // Get connection to database
+        $db = Database::getInstance();
+        $conn = $db->getConnection(); 
 
+        // Find corresponding user logs
+        $user_logs = $conn->query("SELECT * FROM logs;");
+        foreach ($user_logs as $log) {
+            $id_crypt = $log['id_crypt'];
+            if (password_verify($user_id, $id_crypt)) {
+                // Find the log to delete
+                $old_year_crypt = $log['year'];
+                $old_month_crypt = $log['month'];
+                $old_date_crypt = $log['day'];
+                if (    $date == decrypt($old_date_crypt, $user_id) and 
+                        $month == decrypt($old_month_crypt, $user_id) and 
+                        $year == decrypt($old_year_crypt, $user_id)) {
+                    
+                    // Delete the log
+                    $conn->query("DELETE FROM logs WHERE
+                                                   id_crypt = '$id_crypt'
+                                                   and year = '$old_year_crypt' 
+                                                   and month = '$old_month_crypt'
+                                                   and day =  '$old_date_crypt';");
+                    break;
+                }
+            }
+        }  
     }
 
     // Test
@@ -110,5 +136,17 @@
 
     }
 
+    function testDeleteLog() {
+        addLog(6022449822463324, "Happy", "Hunger Acne Bloated", "I won TWO contests today.", 2023, 3, 20);
+        addLog(6022449822463324, "Sad", "Gas Diarrhea", "Hi hello what is up loser.", 2023, 3, 14);
+        addLog(6022449822463324, "Angry", "Bloated Spotting", "Bim Bap.", 2023, 3, 27);
+        addLog(6022449822463324, "Anxious", "Ovu Pain Gas Irritability", "he maximum value depends on the system", 2023, 3, 6);                          
+
+        echo getDashboard(6022449822463324);
+        echo "<br>";
+        deleteLog(6022449822463324, 2023, 3, 27);
+        echo "<br>";
+        echo getDashboard(6022449822463324);
+    }
 ?>
 
